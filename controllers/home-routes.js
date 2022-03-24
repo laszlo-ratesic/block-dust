6364e71a33c58af26caf66d51c2c403d1b4c668e
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-24 13:26:55 -0500

    setup routes, handlebars, and the VC in MVC

---
+++ controllers/home-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    res.render('homepage');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+// const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Turn on Routes
+app.use(routes);
+
+// Turn on connection to server
+app.listen(PORT, () => console.log(`Now listening @ http://localhost:${PORT}`));
