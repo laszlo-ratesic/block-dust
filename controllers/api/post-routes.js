67a5cf7f0fffb2d5825fe04f8908faa92951594f
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-03-26 21:35:56 -0500

    create post-info partial and each loop as well as create post functionality and api routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+
+// GET all posts
+router.get('/', (req, res) => {
+  console.log('================');
+  Post.findAll({
+    // Query configuration
+    attributes: ['id', 'title', 'post_text', 'created_at'],
+    order: [['created_at', 'DESC']],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST /api/posts
+router.post('/', (req, res) => {
+  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  Post.create({
+    title: req.body.title,
+    post_text: req.body.post_text,
+    user_id: req.session.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/feed-routes.js
+++ controllers/feed-routes.js
@@ -4,8 +4,34 @@ const { Post, User, Comment } = require('../models');
 // const withAuth = require('../utils/auth')
 
 router.get('/', (req, res) => {
-    res.render('feed', { loggedIn: req.session.loggedIn });
-    console.log(req.session);
+  console.log(req.session);
+
+  Post.findAll({
+    attributes: ['id', 'title', 'post_text', 'created_at'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  })
+  .then((dbPostData) => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('feed', { posts, loggedIn: req.session.loggedIn });
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+      res.render('error-500');
+  })
 });
 
 module.exports = router;
