a17f7c31b267ff58d071ebe4b58a0f18850ee53f
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-28 11:15:11 -0500

    try something crazy

--- app/seeds/index.js
+++ app/seeds/index.js
@@ -1,6 +1,6 @@
 const sequelize = require('../config/connection');
 const seedUsers = require('./userData');
-const seedPosts = require('./postData');
+const seedPosts = require('./PostData');
 const seedComments = require('./commentData');
 
 const seedAll = async () => {
