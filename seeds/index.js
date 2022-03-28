9a4e8a940a5f690b409a3b3c4f7fb9b5e021e601
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-28 14:37:28 -0500

    temporary rename

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/connection');
 const seedUsers = require('./userData');
-const seedComments = require('./commentData');
 const seedPosts = require('./postData');
+const seedComments = require('./commentData');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
